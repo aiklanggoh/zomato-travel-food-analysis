7ec2616dfd6d8295b2ec9f0aa61dc06954728cab
aiklanggoh <aiklang.goh.2021@scis.smu.edu.sg>
2024-08-30 14:35:09 +0800

    feat: extract_restaurant_data

--- task1.py
+++ task1.py
@@ -17,3 +17,29 @@ def load_country_codes(country_file):
     country_codes = pd.read_excel(country_file)
     country_codes_dict = country_codes.set_index('Country Code')['Country'].to_dict()
     return country_codes_dict
+
+def extract_restaurant_data(data, country_codes_dict):
+    """Extract the required fields from the restaurant data."""
+    extracted_data = []
+    for record in data:
+        for restaurant_record in record['restaurants']:
+            restaurant = restaurant_record['restaurant']
+            restaurant_id = restaurant['id']
+            restaurant_name = restaurant['name']
+            country = restaurant['location']['country_id']
+            city = restaurant['location']['city']
+            user_rating_votes = restaurant['user_rating']['votes']
+            user_aggregate_rating = float(restaurant['user_rating']['aggregate_rating'])
+            cuisines = restaurant['cuisines']
+            if country in country_codes_dict:
+                country = country_codes_dict[country]
+            extracted_data.append([
+                restaurant_id,
+                restaurant_name,
+                country,
+                city,
+                user_rating_votes,
+                user_aggregate_rating,
+                cuisines
+            ])
+    return extracted_data
