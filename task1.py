1d6d99775b47e33e1b3fcef0f9d6a61ce89ca20c
aiklanggoh <aiklang.goh.2021@scis.smu.edu.sg>
2024-08-30 14:33:56 +0800

    feat: load_country_code to extract data from secondary data source

--- task1.py
+++ task1.py
@@ -10,3 +10,10 @@ def load_json_file(input_file):
     with open(input_file, 'r') as file:
         data = json.load(file)
     return data
+
+def load_country_codes(country_file):
+    """Load the country codes from the file into a dictionary using Pandas."""
+    # Convert Country-Code.xlsx to a dictionary
+    country_codes = pd.read_excel(country_file)
+    country_codes_dict = country_codes.set_index('Country Code')['Country'].to_dict()
+    return country_codes_dict
